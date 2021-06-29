7ca11c9ab3a88c27d1e38b13559774ef7e25460a
Aleja-Carbajal <62610164+Aleja-Carbajal@users.noreply.github.com>
2021-06-28 18:26:02 -0600

    Added Charts

---
+++ Proyecto BASES-POO 2021/Programación orientada a objetos/Graficos/GraficosVacunaciones/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace GraficosVacunaciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chtSideEffects = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chtRangeTime = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTotal1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblTotal2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chtSideEffects)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chtRangeTime)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chtSideEffects
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chtSideEffects.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chtSideEffects.Legends.Add(legend1);
+            this.chtSideEffects.Location = new System.Drawing.Point(0, 373);
+            this.chtSideEffects.Name = "chtSideEffects";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series1.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.IsValueShownAsLabel = true;
+            series1.LabelForeColor = System.Drawing.Color.White;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chtSideEffects.Series.Add(series1);
+            this.chtSideEffects.Size = new System.Drawing.Size(720, 404);
+            this.chtSideEffects.TabIndex = 0;
+            this.chtSideEffects.Text = "Efectos secundarios";
+            title1.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.ForeColor = System.Drawing.Color.LightSlateGray;
+            title1.Name = "Title1";
+            title1.Text = "Efectos Secundarios más Frecuentes";
+            this.chtSideEffects.Titles.Add(title1);
+            // 
+            // chtRangeTime
+            // 
+            chartArea2.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea2.Name = "ChartArea1";
+            this.chtRangeTime.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chtRangeTime.Legends.Add(legend2);
+            this.chtRangeTime.Location = new System.Drawing.Point(726, 373);
+            this.chtRangeTime.Name = "chtRangeTime";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series2.Legend = "Legend1";
+            series2.Name = " Ciudadanos Atentidos";
+            this.chtRangeTime.Series.Add(series2);
+            this.chtRangeTime.Size = new System.Drawing.Size(648, 404);
+            this.chtRangeTime.TabIndex = 1;
+            this.chtRangeTime.Text = "chart1";
+            title2.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title2.ForeColor = System.Drawing.Color.LightSlateGray;
+            title2.Name = "Title1";
+            title2.Text = "Tiempo de espera para vacunación";
+            this.chtRangeTime.Titles.Add(title2);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblTotal1);
+            this.groupBox1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.groupBox1.Location = new System.Drawing.Point(0, 187);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(432, 163);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblTotal1
+            // 
+            this.lblTotal1.AutoSize = true;
+            this.lblTotal1.Font = new System.Drawing.Font("Lucida Sans", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal1.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.lblTotal1.Location = new System.Drawing.Point(173, 65);
+            this.lblTotal1.Name = "lblTotal1";
+            this.lblTotal1.Size = new System.Drawing.Size(125, 39);
+            this.lblTotal1.TabIndex = 3;
+            this.lblTotal1.Text = "label1";
+            this.lblTotal1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Teal;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(40, 139);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5);
+            this.label1.Size = new System.Drawing.Size(348, 88);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Personas con una \r\ndosis";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Teal;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(489, 139);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(5);
+            this.label2.Size = new System.Drawing.Size(347, 88);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Personas con dos \r\ndosis";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.White;
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.lblTotal2);
+            this.groupBox2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.groupBox2.Location = new System.Drawing.Point(454, 187);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(422, 163);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // lblTotal2
+            // 
+            this.lblTotal2.AutoSize = true;
+            this.lblTotal2.Font = new System.Drawing.Font("Lucida Sans", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal2.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.lblTotal2.Location = new System.Drawing.Point(160, 65);
+            this.lblTotal2.Name = "lblTotal2";
+            this.lblTotal2.Size = new System.Drawing.Size(125, 39);
+            this.lblTotal2.TabIndex = 3;
+            this.lblTotal2.Text = "label1";
+            this.lblTotal2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Teal;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(925, 139);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(5);
+            this.label3.Size = new System.Drawing.Size(384, 88);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Dosis suministradas\r\nen total";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.White;
+            this.groupBox3.Controls.Add(this.lblTotal);
+            this.groupBox3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.groupBox3.Location = new System.Drawing.Point(901, 187);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(443, 163);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Lucida Sans", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.lblTotal.Location = new System.Drawing.Point(179, 65);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(143, 45);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "label1";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(119, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1086, 55);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Estadísticas de vacunación contra Covid 19";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label5.Location = new System.Drawing.Point(90, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(242, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Vacunadas completamente";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label6.Location = new System.Drawing.Point(87, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(221, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Solamente con una dosis";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1372, 777);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chtRangeTime);
+            this.Controls.Add(this.chtSideEffects);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Estadísticas Vacuna COVID 19";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chtSideEffects)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chtRangeTime)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtSideEffects;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtRangeTime;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblTotal1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblTotal2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Proyecto BASES-POO 2021/Programación orientada a objetos/Graficos/GraficosVacunaciones/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace GraficosVacunaciones
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection Connection = new SqlConnection("Server=localhost;Database=FinalProject;Trusted_Connection=True;");
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        // PARA COLUMNAS PARA GRAFICO DE TIEMPO DE ESPERA
+        ArrayList Range = new ArrayList();
+        ArrayList CitizenNum = new ArrayList();
+        // PARA COLUMNAS PARA GRAFICO DE EFECTOS
+        ArrayList SideEffect = new ArrayList();
+        ArrayList NumCitizen = new ArrayList();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChartRangeTime();
+            ChartSideEffects();
+            DashboardDatos();
+        }
+
+        private void ChartRangeTime()
+        {
+            cmd = new SqlCommand("MinutesRanges", Connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            Connection.Open();
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Range.Add(dr.GetString(0));
+                CitizenNum.Add(dr.GetInt32(1));
+            }
+            chtRangeTime.Series[0].Points.DataBindXY(Range, CitizenNum);
+            dr.Close();
+            Connection.Close();
+        }
+
+        private void ChartSideEffects()
+        {
+            cmd = new SqlCommand("SideEffectShowed", Connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            Connection.Open();
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                SideEffect.Add(dr.GetString(0));
+                NumCitizen.Add(dr.GetInt32(1));
+            }
+            chtSideEffects.Series[0].Points.DataBindXY(SideEffect, NumCitizen);
+            dr.Close();
+            Connection.Close();
+        }
+
+        private void DashboardDatos()
+        {
+            cmd = new SqlCommand("DashboardData", Connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter total1 = new SqlParameter("@totalPeople1Dossis", 0); total1.Direction = ParameterDirection.Output;
+            SqlParameter total2 = new SqlParameter("@totalPeople2Dossis", 0); total2.Direction = ParameterDirection.Output;
+            SqlParameter total = new SqlParameter("@totalPeopleVaccinated", 0); total.Direction = ParameterDirection.Output;
+
+            cmd.Parameters.Add(total1);
+            cmd.Parameters.Add(total2);
+            cmd.Parameters.Add(total);
+            Connection.Open();
+            cmd.ExecuteNonQuery();
+            lblTotal1.Text = cmd.Parameters["@totalPeople1Dossis"].Value.ToString();
+            lblTotal2.Text = cmd.Parameters["@totalPeople2Dossis"].Value.ToString();
+            lblTotal.Text = cmd.Parameters["@totalPeopleVaccinated"].Value.ToString();
+            Connection.Close();
+        }
+    }
+}
